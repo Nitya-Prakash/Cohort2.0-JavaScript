b456d4645c33dcf3ca1b21acf207a07c88e2fd31
Nitya-Prakash <nityaprakashsahoo6776@gmail.com>
2025-11-03 18:41:55 +0530

    Day4 Class Notes

--- Day-4/script.js
+++ Day-4/script.js
@@ -1,14 +1,45 @@
 // Opeartors in js
 
-// Assignment operators -
+// 1. Airthmetc Operators -
+// + - * / % **
 
-// = :
+// 2. Assignment operators -
 
-// Comparison operators -
-// == ===
+// = += -= *= /= %=
+let x = 2;
+// x = x+2; // or
+// x += 2;
+// x -= 2;
+// x *= 2;
+// x /= 2;
+// x %= 2;
+
+// 3. Comparison operators -
+// == === != !== < > >= <=
+
+// == not strict compare NEVER USE THIS
+// === strict compare ALWAYS USE THIS
+// != NEVER USE THIS
+// !== ALWAYS USE THIS
+// >
+// <
+// >=
+// <=
+
+// 4. Logical Operators
+// && || !
 
 // && - Dono side ko sahi hona padega
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
 // || - Koi bhi ek side agar sahi ho gaya then also it will work
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
 
 // ! - true ko false kardega and vice-versa
 
@@ -18,24 +49,45 @@
 //   console.log("If condition will run.");
 // }
 
-// Ternary Operator -
+// 5. Ternary Operator -
 // condition ? do this : do that
 // 12 > 13 ? console.log("Hello!") : console.log("Hey"); // Hey
 
-// Type checking operators -
+// 6. Type checking operators -
 // typeof
 // it gives you the type of a particular thing.
 
 // instanceof
 
-let arr = 12;
+// let arr = 12;
 // arr instanceof Array // true
 // Kuch bhi banaoge , wo top pe likhi gai cheezon se aata hai . It means if you assign a value 12 then it is an instance of that top level Number class.
 
-// String operator -
+// 7. String operator -
 // + (concatination)
 
-// Spread and Rest Operator
-// ...
+// 8. Spread and Rest Operator
 
-// Hoisting in const
+// Spread (...)
+// let arr = [1, 2, 3, 4];
+// let arr2 = [...arr];
+
+// Rest (...) (Specially used for functions)
+
+// function abc(a, b, c, ...rest) {
+//   console.log(...rest); // 4 5
+// }
+// abc(1, 2, 3, 4, 5);
+
+// 9. Nullish Coalescing Operator
+// ?? (fallback only when null / undefined)
+
+// 12 > 13 ?? console.log("sorry"); // false
+// null ?? console.log("sorry"); // sorry
+
+// 10. Optional chaining
+// ?. (safe access to nested values)
+
+// let obj = {};
+// obj?.name?.first;
+// So if the nested values are present then it will give the answers , if not then it will not give any errors
